c55778be56d350cbe211bec149e9b09b7e2c6f36
wh0507 <banggu0507@gmail.com>
2020-12-01 15:03:56 +0900

    復習写真投稿実装

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,19 +1,43 @@
-document.addEventListener('DOMContentLoaded', function(){
-  const ImageList = document.getElementById('image-list');
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function() {
+    const ImageList = document.getElementById('image-list')
 
-  document.getElementById('message_image').addEventListener('change', function(e){
-    const file = e.target.files[0];
-    const blob = window.URL.createObjectURL(file);
+    // 選択した画像を表示する関数
+    const createImageHTML = (blob) => {
+      // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div')
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
 
-    // 画像を表示するためのdiv要素を生成
-    const imageElement = document.createElement('div');
+      // 表示する画像を生成
+      const blobImage = document.createElement('img')
+      blobImage.setAttribute('src', blob)
 
-    // 表示する画像を生成
-    const blobImage = document.createElement('img');
-    blobImage.setAttribute('src', blob);
+      // ファイル選択ボタンを生成
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `message_image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'message[images][]')
+      inputHTML.setAttribute('type', 'file')
 
-  // 生成したHTMLの要素をブラウザに表示させる
-    imageElement.appendChild(blobImage);
-    ImageList.appendChild(imageElement);
+      // 生成したHTMLの要素をブラウザに表示させる
+      imageElement.appendChild(blobImage)
+      imageElement.appendChild(inputHTML)
+      ImageList.appendChild(imageElement)
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob)
+      })
+    }
+
+    document.getElementById('message_image').addEventListener('change', (e) => {
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob)
+
+    });
   });
-});
+}
